81b0779254fd0e8976133786730a280c2a16a78c
unknown <zachary.morin-adm@ZACH.LOCAL>
2022-02-23 02:42:43 -0500

    Added secure-ssh again

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -1,7 +1,10 @@
 echo $1
-adduser $1 
-usermod -aG wheel $1
-passwd $1
+echo $2
+sudo adduser "$1"
+sudo usermod -aG wheel $1
+sudo echo "$2" | sudo passwd "$1" --stdin
+su - $1 -c "ssh-keygen -t rsa -C 'test'"
+ssh -t localadmin@10.0.5.151 "sudo adduser '$1' && sudo usermod -aG sudo $1"
+su - $1 -c "ssh-copy-id $1@10.0.5.151"
 sed -i 's/#PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
 cd /home/localadmin/.ssh/
-scp localadmin@10.0.5.151:/home/localadmin/.ssh/id_rsa /home/$1/.ssh/
